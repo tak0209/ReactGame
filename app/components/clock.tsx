d1e67f13f6e8b91bdaa5f811d04423934f3e161b
Tak Ng <tang@microsoft.com>
2016-07-14 00:56:23 -0700

    Add event on timer component, bootstrap carousel

--- app/components/clock.tsx
+++ app/components/clock.tsx
@@ -1,24 +1,31 @@
 import * as React from "react";
 
-class elapsedState {
+class timerState {
     secondsElapsed: number;
+    bgColor: string;
+    caption: string;
 }
-export class Timer extends React.Component<void, elapsedState>{
-    interval : any;
+
+export class Timer extends React.Component<void, timerState>{
+    interval: number;
+    isTimerOn: boolean;
     constructor() {
         super();
         // set initial state
-        this.state = { secondsElapsed: 100 };
+        this.state = {
+            secondsElapsed: 100,
+            bgColor: 'white',
+            caption: 'off'
+        };
+        this.isTimerOn = true;
     }
 
     tick = () => {
-        {
-            this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });
-        }
+        this.setState({ secondsElapsed: this.state.secondsElapsed + 1, bgColor: 'white', caption: 'off' });
     };
 
     componentDidMount() {
-       this.interval = setInterval(this.tick, 1000);
+        this.interval = setInterval(this.tick, 1000);
         //setInterval(()=>{ this.setState({secondsElapsed: this.state.secondsElapsed + 1}) }, 1000);
     };
 
@@ -26,9 +33,24 @@ export class Timer extends React.Component<void, elapsedState>{
         clearInterval(this.interval);
     };
 
+    handleClick = () => {
+        if (this.isTimerOn) {
+            clearInterval(this.interval);
+            this.setState({ secondsElapsed: this.state.secondsElapsed, bgColor: 'lightgrey', caption: 'on' });
+        }
+        else {
+            this.interval = setInterval(this.tick, 1000);
+            this.setState({ secondsElapsed: this.state.secondsElapsed, bgColor: 'white', caption: 'off' });
+        }
+        this.isTimerOn = !this.isTimerOn;
+    }
+
     render() {
         return (
-            <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
+            <div style={{ fontSize: 2 + 'em', backgroundColor: this.state.bgColor }}>Seconds Elapsed: {this.state.secondsElapsed}
+                <span style={{ paddingLeft: '15px' }} ></span>
+                <button class="btn btn-lg btn-primary" type="button" onClick={this.handleClick}>{this.state.caption}</button>
+            </div>
         );
     }
 }
--- app/components/hello.tsx
+++ app/components/hello.tsx
@@ -4,7 +4,7 @@ export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1 style={{ fontSize: 2 + 'em', backgroundColor: 'yellow' }}>
+        return <h1 style={{ fontSize: 2 + 'em', backgroundColor: 'white' }}>
             Hi from {this.props.compiler} and {this.props.framework}.This is what it looks like</h1>;
     }
 }   
